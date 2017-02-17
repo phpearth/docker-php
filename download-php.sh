a8cfd867f019bbcc572b0fc04af4731727aae27e
Peter Kokot <peterkokot@gmail.com>
2017-02-17 20:27:57 +0100

    Upgrade PHP to 7.1.2

--- download-php.sh
+++ download-php.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# PHP version can be passed as an argument for example ./download-php 7.1.1
-PHP_VERSION="${1:-7.1.1}"
+# PHP version can be passed as an argument for example ./download-php 7.1.0
+PHP_VERSION="${1:-7.1.2}"
 GPG_KEY_1="A917B1ECDA84AEC2B568FED6F50ABC807BD5DCD0"
 GPG_KEY_2="528995BFEDFBA7191D46839EF9BA0ADA31CBD89E"
 PHP_URL="https://secure.php.net/get/php-$PHP_VERSION.tar.xz/from/this/mirror"
 PHP_ASC_URL="https://secure.php.net/get/php-$PHP_VERSION.tar.xz.asc/from/this/mirror"
-PHP_SHA256="b3565b0c1441064eba204821608df1ec7367abff881286898d900c2c2a5ffe70"
-PHP_MD5="65eef256f6e7104a05361939f5e23ada"
+PHP_SHA256="d815a0c39fd57bab1434a77ff0610fb507c22f790c66cd6f26e27030c4b3e971"
+PHP_MD5="d79afea1870277c86fac903566fb6c5d"
 
 curl -o php.tar.xz -OL $PHP_URL
 if [ -n "$PHP_SHA256" ]; then
